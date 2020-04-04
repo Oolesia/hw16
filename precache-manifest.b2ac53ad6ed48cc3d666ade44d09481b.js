a8cea5217731de8baf8ac1002dafda01b25ae8a3
Olesia Sikora <sikora0310@gmail.com>
2020-04-04 11:40:18 +0300

    Updates

---
+++ precache-manifest.b2ac53ad6ed48cc3d666ade44d09481b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0838f083c308a5982a2f0ad86a8de3",
+    "url": "/hw16/index.html"
+  },
+  {
+    "revision": "0492d1c6dc4616cedc23",
+    "url": "/hw16/static/css/main.1761964d.chunk.css"
+  },
+  {
+    "revision": "a4a385ace3d7c7e1b211",
+    "url": "/hw16/static/js/2.7b54caf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw16/static/js/2.7b54caf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0492d1c6dc4616cedc23",
+    "url": "/hw16/static/js/main.8fb8702b.chunk.js"
+  },
+  {
+    "revision": "63265d46e0b41a84e7da",
+    "url": "/hw16/static/js/runtime-main.1ae2a683.js"
+  },
+  {
+    "revision": "5c87994c964d115c484b909be298743b",
+    "url": "/hw16/static/media/female.5c87994c.svg"
+  },
+  {
+    "revision": "075829956d01b4f9d072ca02aa84e2f2",
+    "url": "/hw16/static/media/male.07582995.svg"
+  }
+]);
